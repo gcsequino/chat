4dbb4ecd94270ac27d0c97578ab2bea5c4307407
gcsequino <gcsequino@gmail.com>
2022-10-13 10:27:29 -0600

    test server done

--- chat.cpp
+++ chat.cpp
@@ -9,13 +9,18 @@
 * Member2 SIS ID: XXXXXX
 * Member2 Login ID: XXXXXX
 ********************************************/
-#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 #include <string.h>
+#include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
 #include <time.h>
 
 #define MYPORT "3490" // the port users will be connecting to
@@ -31,6 +36,19 @@ int client() {
   return 0;
 }
 
+void sigchld_handler(int s){
+  int save_errno = errno;
+  while(waitpid(-1, NULL, WNOHANG) > 0);
+  errno = save_errno;
+}
+
+void *get_in_addr(struct sockaddr *sa){
+  if(sa->sa_family == AF_INET){
+    return &(((struct sockaddr_in*)sa)->sin_addr); // IPV$
+  }
+  return &(((struct sockaddr_in6*)sa)->sin6_addr); // IPV6
+}
+
 int server() {
   // set up TCP port and listen for connection
   // print out IP and Port
@@ -40,33 +58,87 @@ int server() {
   //   revieve and print message
   //   prompt user for message to send
   //   send message to client
-
-  struct sockaddr_storage their_addr;
-  socklen_t addr_size;
-  struct addrinfo hints, *res;
   int sockfd, new_fd;
+  struct addrinfo hints, *servinfo, *p;
+  struct sockaddr_storage their_addr;
+  socklen_t sin_size;
+  struct sigaction sa;
+  int yes=1;
+  char s[INET6_ADDRSTRLEN];
+  int rv;
 
   memset(&hints, 0, sizeof hints);
-  hints.ai_family = AF_INET;
+  hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_flags = AI_PASSIVE;
 
-  getaddrinfo(NULL, MYPORT, &hints, &res);
+  if ((rv = getaddrinfo(NULL, MYPORT, &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return 1;
+  }
 
-  sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-  bind(sockfd, res->ai_addr, res->ai_addrlen);
-  listen(sockfd, BACKLOG);
+  for(p = servinfo; p != NULL; p = p->ai_next){
+    if((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1){
+      perror("server: socket");
+      continue;
+    }
+    if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1){
+      perror("setsockopt");
+      exit(1);
+    }
+    if(bind(sockfd, p->ai_addr, p->ai_addrlen) == -1){
+      close(sockfd);
+      perror("server: bind");
+      continue;
+    }
+    break;
+  }
 
-  addr_size = sizeof their_addr;
-  new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
+  freeaddrinfo(servinfo);
 
-  char msg[] = "This is Greyson's server";
-  int len = strlen(msg);
-  int bytes_sent;
+  if(p == NULL){
+    fprintf(stderr, "server: failed to bind\n");
+    exit(1);
+  }
 
-  bytes_sent = send(sockfd, msg, len, 0);
+  if(listen(sockfd, BACKLOG) == -1){
+    perror("listen");
+    exit(1);
+  }
 
-  close(sockfd);
+  sa.sa_handler = sigchld_handler;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART;
+  if(sigaction(SIGCHLD, &sa, NULL) == -1){
+    perror("sigaction");
+    exit(1);
+  }
+
+  printf("server: waiting for connections...\n");
+
+  while(1){
+    sin_size = sizeof their_addr;
+    new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+    if(new_fd == -1){
+      perror("accept");
+      continue;
+    }
+
+    inet_ntop(their_addr.ss_family,
+              get_in_addr((struct sockaddr *)&their_addr),
+              s, sizeof s);
+    printf("server: get connection from %s\n", s);
+
+    if(!fork()){
+      close(sockfd);
+      if(send(new_fd, "Hello from server!", 18, 0) == -1){
+        perror("send");
+      }
+      close(new_fd);
+      exit(0);
+    }
+    close(new_fd);
+  }
 
   return 0;
 }
